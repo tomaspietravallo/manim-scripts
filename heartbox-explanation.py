ff3817345bec494df85ee2ab8ab83973b4cd1026
Tomas Pietravallo <62444821+tomaspietravallo@users.noreply.github.com>
2020-06-30 20:19:23 -0300

    Update

--- heartbox-explanation.py
+++ heartbox-explanation.py
@@ -22,13 +22,31 @@ class ExplanationPartOne(Scene):
 
             equations = [
                 TexMobject(r'l', r'= \sqrt{a^{2} + a^{2}}'), #0
-                TexMobject(r'l', r'= {{1\over2}\times {1\over2}}\times \sqrt{a^{2} + a^{2}}'), #1
+                TexMobject(r'l', r'= {3\over4}\times \sqrt{a^{2} + a^{2}}'), #1
                 TexMobject(r'l', r'= {1\over4}\times \sqrt{a^{2} + a^{2}}'), #2
                 TexMobject(r'l', r'= {1\over4}\times \sqrt{2a^{2}}'), #3
                 TexMobject(r'l', r'= {1\over4}\times \sqrt{2} \times a'), #4
                 TexMobject(r'l', r'= {\sqrt{2} \times a \over 4}'), #5
-                TexMobject(r'\text{Area}', r'= {\sqrt{2} \times a \over 4} \times {\sqrt{2} \times a \over 4}') #6
             ]
+            first_fold =[(0,1.4,0),
+                        (1.06,0.21,0),
+                        (1.06,-0.21,0),
+                        (0,-1.4,0),
+                        (-1.06,-0.21,0),
+                        (-1.06,0.21,0),
+                        (0,1.4,0)
+                        ]
+            second_fold =[(0,1.4,0),
+                        (0.34, 1.2,0),
+                        (0.34, -1.2,0),
+                        (0,-1.4,0),
+                        (-0.34, -1.2,0),
+                        (-0.34, 1.2,0),
+                        (0, 1.4,0)
+                        ]
+
+            poly_first_fold = Polygon(*first_fold)
+            poly_second_fold = Polygon(*second_fold)
             duplicate_eq_5 = TexMobject(r'l', r'= {\sqrt{2} \times a \over 4}')
             duplicate_eq_5[0].set_color(YELLOW)
             square = Square()
@@ -71,7 +89,7 @@ class ExplanationPartOne(Scene):
                 Rotate(line, angle= -45*DEGREES),
             )
 
-            self.play(FadeOut(square)) # Fade out square
+            # self.play(FadeOut(square)) # Fade out square
 
             self.wait()
 
@@ -80,13 +98,12 @@ class ExplanationPartOne(Scene):
                 equations[index][0].set_color(YELLOW)
 
             # 1/2 The line twice
-            line_halve = Line((-0.7, 0, 0), (0.7, 0, 0), color=YELLOW)
+            line_three_fourths = Line((-1.06, 0, 0), (1.06, 0, 0), color=YELLOW)
             line_halve_twice = Line((-0.35, 0, 0), (0.35, 0, 0), color=YELLOW)
             self.play(ReplacementTransform(after_hypotenuse, equations[1]))
-            self.wait(0.3)
-            self.play(Transform(line, line_halve))
-            self.wait(0.3)
-            self.play(Transform(line, line_halve_twice))
+            self.play(Transform(line, line_three_fourths), ReplacementTransform(square, poly_first_fold))
+            self.wait(0.6)
+            self.play(Transform(line, line_halve_twice), ReplacementTransform(poly_first_fold, poly_second_fold), ReplacementTransform(equations[1], equations[2]))
             self.wait(0.5)
             #############
             
@@ -94,7 +111,7 @@ class ExplanationPartOne(Scene):
 
             # Nothing important part of the system
             for index, equation in enumerate(equations):
-                if index <= 1:
+                if index <= 2:
                     pass
                 elif index < 5:
                     self.play(ReplacementTransform(equations[index - 1], equation))
@@ -107,20 +124,96 @@ class ExplanationPartOne(Scene):
             self.play(ShowCreationThenDestruction(highlighter))
             self.wait(0.3)
 
-            self.play(ApplyMethod(equations[5].move_to, 0*UP), FadeOut(line))
-            duplicate = equations[5]
-            self.add(duplicate_eq_5)
-            self.wait()
-
-            highlighter = SurroundingRectangle(equations[6], color=BLUE)
-            self.play(ReplacementTransform(equations[5], equations[6]))
-            self.wait(0.3)
-
-            self.play(ShowCreationThenDestruction(highlighter))
+            # This end sequence is MESSY, it has errors, it wasn't used
+            final_square = Square(color=BLUE).scale(0.35355)
+            self.play(ReplacementTransform(poly_second_fold, final_square), ApplyMethod(line.next_to, args=[final_square, DOWN], buff=0))
+            # duplicate = equations[5]
+            # self.add(duplicate_eq_5)
             self.wait()
 
         draw()
         
         pass
 
+
+class Intro(Scene):
+    def construct(self):
+        title = TextMobject("Problema N°4 - Caja Corazon")
+        subTitle = TextMobject("Constanza Etchebere y Tomas Pietravallo")\
+            .to_edge(DOWN)\
+            .scale(0.45)
+        self.play(Write(title, run_time=2.0), Write(subTitle, run_time=3))
+        self.wait(0.3)
+        self.play(FadeOut(title), FadeOut(subTitle))
+
+class ShowSquare(Scene):
+    def construct(self):
+        square = Square()
+        line = Line((-1,-1,0), (1,1,0), color=YELLOW)
+        label_y = TexMobject("a").next_to(square, LEFT)
+        label_x = TexMobject("a").next_to(square, DOWN)
+        text_x = TexMobject(r"\times")
+        text_x.move_to(2.0*DOWN)
+        after_hypotenuse = TexMobject(r'l', r'= \sqrt{a^{2} + a^{2}}')
+        after_hypotenuse[0].set_color(YELLOW)
+        after_hypotenuse.move_to(2.0*DOWN)
+
+        title = TextMobject('Definimos el problema').move_to(2*UP)
+        self.play(Write(title)) # Title
+
+        self.play(ShowCreation(square)) # Square
+
+        self.play(Write(label_x), Write(label_y)) # Show labels
+
+        self.wait(0.3)
+
+        self.play( # Move labels  & write LateX '\times'
+            Write(text_x),
+            ApplyMethod(label_y.next_to, text_x, LEFT),
+            ApplyMethod(label_x.next_to, text_x, RIGHT),
+        )
+
+        self.wait(0.3)
+
+        title_reference = TextMobject("Tomamos como referencia la hipotenusa").move_to(2*UP)
+
+        self.play( # Show hypotenuse and transfrom labels/eq
+            ShowCreation(line), 
+            FadeOut(label_y), 
+            FadeOut(label_x), 
+            ReplacementTransform(text_x, after_hypotenuse),
+            ReplacementTransform(title, title_reference)
+        )
+
+        self.wait(0.5)
+
+        self.play( # Rotate square and line
+            Rotate(square, angle= -45*DEGREES),
+            Rotate(line, angle= -45*DEGREES),
+        )
+        
+        self.wait()
+
+
+class LineTransform(Scene):
+    def construct(self):
+        title_reference = TextMobject("Tomamos como referencia la hipotenusa").move_to(2*UP)
+        square = Square().rotate(-45*DEGREES)
+        line = Line((-1,-1,0), (1,1,0), color=YELLOW).rotate(-45*DEGREES)
+        after_hypotenuse = TexMobject(r'l', r'= \sqrt{a^{2} + a^{2}}')
+        after_hypotenuse[0].set_color(YELLOW)
+        after_hypotenuse.move_to(2.0*DOWN)
+
+        self.add( # Match previous scene
+            title_reference,
+            square,
+            line,
+            after_hypotenuse
+        )
 # python3 -m manim manim-scripts/heartbox-explanation.py ExplanationPartOne -pl --media_dir "/Users/tomaspietravallo/Desktop/quarantine-stuff/manim-output"
+
+# python3 -m manim manim-scripts/heartbox-explanation.py Intro -p --media_dir "/Users/tomaspietravallo/Desktop/quarantine-stuff/manim-output"
+
+# python3 -m manim manim-scripts/heartbox-explanation.py ShowSquare -p --media_dir "/Users/tomaspietravallo/Desktop/quarantine-stuff/manim-output"
+
+# python3 -m manim manim-scripts/heartbox-explanation.py LineTransform -p --media_dir "/Users/tomaspietravallo/Desktop/quarantine-stuff/manim-output"
